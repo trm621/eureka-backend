942f2d75f749624d4bd5b2bf5c10797e87b18c15
trm621 <t.maher621@gmail.com>
2021-12-12 16:47:31 -0500

    committing changes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,21 +1,16 @@
 const router = require('express').Router();
-const createNewNote = require('../../lib/notes')
+const createNewNote = require('../../lib/notes');
 const { notes } = require('../../Develop/db/db.json')
 
 router.get('/api/notes', (req, res) => {
-    let results = notes;
-    results = getAndRenderNotes(req, results)
-
-    res.json(results);
+    res.json(notes);
 });
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
 
     const note = createNewNote(req.body, notes)
-  
-      res.json(note)
-    
+    res.json(note)
   });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
@@ -8,7 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
+app.use('/api/notes', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
