b7b7cb0c603cc8e37f5aaecd7e508077108bc158
trm621 <t.maher621@gmail.com>
2021-12-14 19:19:29 -0500

    got notes to save and files to sync

--- db/notes.js
+++ db/notes.js
@@ -7,37 +7,37 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Notes {
 
-read() {
-    return readFileAsync("db/db.json", "utf-8")
-};
+    read() {
+        return readFileAsync("db/db.json", "utf-8")
+    };
 
-write(note) {
-    return writeFileAsync("db/db.json", JSON.stringify(note))
-}
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
 
-getNotes() {
-    return this.read().then(notes => {
-        let notesArray;
+    getNotes() {
+        return this.read().then(notes => {
+            let notesArray;
 
-        try {
-            notesArray = [].concat(JSON.parse(notes))
-        }
-        catch (err) {
-            notesArray = [];
-        }
-        return notesArray;
-    });
-};
+            try {
+                notesArray = [].concat(JSON.parse(notes))
+            }
+            catch (err) {
+                notesArray = [];
+            }
+            return notesArray;
+        });
+    };
 
-addNotes(newNote) {
+    addNotes(newNote) {
 
-    newNote.id = uuidv4();
+        newNote.id = uuidv4();
 
-    return this.getNotes().then(notesArray => {
-        notesArray.push(newNote);
-        this.write(notesArray)
-    })
-}
-}
+        return this.getNotes().then(notesArray => {
+            notesArray.push(newNote);
+            this.write(notesArray)
+        })
+    }
+    }
 
 module.exports = new Notes();
